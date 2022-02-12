f19df2a764c95e82d74b3c372ca2be44fd990aef
Lars901 <larsove_hoines@outlook.com>
2022-02-12 19:45:30 +0100

    Update Format disks.sh

--- Format disks.sh
+++ Format disks.sh
@@ -63,7 +63,20 @@ echo -ne "
 -------------------------------------------------------------------------
                     Creating Filesystems
 -------------------------------------------------------------------------
+"
+createsubvolumes () {
+    btrfs subvolume create /mnt/@
+    btrfs subvolume create /mnt/@home
+    btrfs subvolume create /mnt/@var
+    btrfs subvolume create /mnt/@tmp
+    btrfs subvolume create /mnt/@.snapshots
+}
 
+mountallsubvol () {
+    mount -o ${mountoptions},subvol=@home /dev/mapper/ROOT /mnt/home
+    mount -o ${mountoptions},subvol=@tmp /dev/mapper/ROOT /mnt/tmp
+    mount -o ${mountoptions},subvol=@.snapshots /dev/mapper/ROOT /mnt/.snapshots
+    mount -o ${mountoptions},subvol=@var /dev/mapper/ROOT /mnt/var
 }
 
 if [[ "${DISK}" =~ "nvme" ]]; then
@@ -74,6 +87,10 @@ else
     partition3=${DISK}3
 fi
 
+if [[ "${FS}" == "btrfs" ]]; then
+    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+    mkfs.btrfs -L ROOT ${partition3} -f
+    mount -t btrfs ${partition3} /mnt
 elif [[ "${FS}" == "ext4" ]]; then
     mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
     mkfs.ext4 -L ROOT ${partition3}
@@ -84,10 +101,29 @@ elif [[ "${FS}" == "luks" ]]; then
     echo -n "${luks_password}" | cryptsetup -y -v luksFormat ${partition3} -
 # open luks container and ROOT will be place holder 
     echo -n "${luks_password}" | cryptsetup open ${partition3} ROOT -
+# now format that container
+    mkfs.btrfs -L ROOT /dev/mapper/ROOT
+# create subvolumes for btrfs
+    mount -t btrfs /dev/mapper/ROOT /mnt
+    createsubvolumes       
+    umount /mnt
+# mount @ subvolume
+    mount -o ${mountoptions},subvol=@ /dev/mapper/ROOT /mnt
+# make directories home, .snapshots, var, tmp
+    mkdir -p /mnt/{home,var,tmp,.snapshots}
+# mount subvolumes
+    mountallsubvol
 # store uuid of encrypted partition for grub
     echo encryped_partition_uuid=$(blkid -s UUID -o value ${partition3}) >> setup.conf
 fi
 
+# checking if user selected btrfs
+if [[ ${FS} =~ "btrfs" ]]; then
+ls /mnt | xargs btrfs subvolume delete
+btrfs subvolume create /mnt/@
+umount /mnt
+mount -t btrfs -o subvol=@ -L ROOT /mnt
+fi
 
 # mount target
 mkdir /mnt/boot
@@ -100,5 +136,40 @@ if ! grep -qs '/mnt' /proc/mounts; then
     echo "Rebooting in 2 Seconds ..." && sleep 1
     echo "Rebooting in 1 Second ..." && sleep 1
     reboot now
+fi
+echo -ne "
+-------------------------------------------------------------------------
+                    Arch Install on Main Drive
+-------------------------------------------------------------------------
+"
+pacstrap /mnt base base-devel linux linux-firmware vim nano sudo archlinux-keyring wget libnewt --noconfirm --needed
+echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
+cp -R ${SCRIPT_DIR} /mnt/root/ArchTitus
+cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+echo -ne "
+-------------------------------------------------------------------------
+                    GRUB BIOS Bootloader Install & Check
+-------------------------------------------------------------------------
+"
+if [[ ! -d "/sys/firmware/efi" ]]; then
+    grub-install --boot-directory=/mnt/boot ${DISK}
+fi
+echo -ne "
+-------------------------------------------------------------------------
+                    Checking for low memory systems <8G
+-------------------------------------------------------------------------
+"
+TOTALMEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
+if [[  $TOTALMEM -lt 8000000 ]]; then
+    # Put swap into the actual system, not into RAM disk, otherwise there is no point in it, it'll cache RAM into RAM. So, /mnt/ everything.
+    mkdir /mnt/opt/swap # make a dir that we can apply NOCOW to to make it btrfs-friendly.
+    chattr +C /mnt/opt/swap # apply NOCOW, btrfs needs that.
+    dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=2048 status=progress
+    chmod 600 /mnt/opt/swap/swapfile # set permissions.
+    chown root /mnt/opt/swap/swapfile
+    mkswap /mnt/opt/swap/swapfile
+    swapon /mnt/opt/swap/swapfile
+    # The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the system itself.
+    echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab # Add swap to fstab, so it KEEPS working after installation.
 fi
 echo -ne "
